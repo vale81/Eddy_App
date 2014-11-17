8c0184e50b7d1c90e0fe619c03d9c48a54d62789
vale81 <tim.hucke@gmail.com>
2014-11-17 21:45:49 +0100

    Datenbank Version 1 implementiert ToDo: Echte implementierung return data (alle werte) verbesserung eintrag formular damit alle nötigen einträge gespeichert werden können

--- app/src/main/java/com/example/fabian/myapplication/EintragFormular.java
+++ app/src/main/java/com/example/fabian/myapplication/EintragFormular.java
@@ -1,19 +1,85 @@
 package com.example.fabian.myapplication;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Fabian on 09.11.2014.
  */
 public class EintragFormular extends Activity{
 
+    private EditText usersBz;
+    private Button save_Button;
+    private TextView bzdata;
+
+
+    // Um Datenbank öffnen zu können brauchen wir ein DataHandler Objekt
+    DataHandler the_handler;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.eintrag_formular);
         // The activity is being created.
+
+       //init Users BZ
+        usersBz = (EditText) findViewById(R.id.users_Bz);
+       // init Save Button
+        save_Button = (Button) findViewById(R.id.save_button);
+        // INIt test textview clickable
+        bzdata = (TextView) findViewById(R.id.bzbzbz);
+
+
+
+        bzdata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String derWert = " ";
+                // Handler objekt anlegen
+                the_handler = new DataHandler(getBaseContext());
+                the_handler.open();
+                Cursor c = the_handler.returnData();
+                // Falls Daten vorhanden setze BZwert auf String an Index stelle 0
+                // Index startet bei 0, jeder Eintrag (Bolus Basis etc) an entsprechender Stelle
+                // muss noch erweitert werden soll nur grundfunktion testen
+                if(c.moveToFirst())
+                {
+                    derWert = c.getString(0);
+                }
+                bzdata.setText("BZ:" + derWert);
+                the_handler.closeDatabase();
+                Toast.makeText(getBaseContext(), "BZ WERT:" + derWert, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        save_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getBZWert = usersBz.getText().toString();
+                the_handler = new DataHandler(getBaseContext());
+                the_handler.open();
+                the_handler.insertData(getBZWert, null, null, null, null, null, null);
+                Toast.makeText(getBaseContext(), "Data inserted", Toast.LENGTH_LONG).show();
+                the_handler.closeDatabase();
+            }
+        });
     }
+
+    // Test BZ Übergabe NUR UM DATENBANK zu TESTEN später löschen!!!!!
+
+
+
+
+
+
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -39,5 +105,4 @@ public class EintragFormular extends Activity{
         super.onDestroy();
         // The activity is about to be destroyed.
     }
-
 }
